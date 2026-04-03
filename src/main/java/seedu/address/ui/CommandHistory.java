7dd35318d05c0bf769f023d12a88fd43857d957b
e1384467 <e1384467@u.nus.edu>
2026-04-03 18:26:03 +0800

    Add duplicate command handling in history

--- src/main/java/seedu/address/ui/CommandHistory.java
+++ src/main/java/seedu/address/ui/CommandHistory.java
@@ -13,14 +13,33 @@ public class CommandHistory {
 
     }
 
+    /**
+     * Adds a command to the history. If the command is a duplicate of an existing command, the existing command
+     * is removed and the new command is added to the end of the history.
+     */
     public void addCommand(String command) {
         assert command != null;
+        if(isDuplicateCommand(command)) {
+            removeAllDuplicateCommand(command);
+        }
         commandHistory.add(command);
         historyIndex = commandHistory.size();
         currentInput="";
 
     }
 
+    private void removeAllDuplicateCommand(String command) {
+        commandHistory.removeIf(c -> c.equalsIgnoreCase(command));
+    }
+
+    private boolean isDuplicateCommand(String command) {
+        return commandHistory.contains(command);
+    }
+
+
+    /**
+     * Returns the previous command in the history. If there is no previous command, returns the current input.
+     */
     public String getPreviousCommand(String currentInput) {
         assert currentInput != null;
         if (commandHistory.isEmpty()) {
@@ -39,6 +58,9 @@ public class CommandHistory {
         return commandHistory.get(historyIndex);
     }
 
+    /**
+     * Returns the next command in the history. If there is no next command, returns the current input.
+     */
     public String getNextCommand() {
         if (commandHistory.isEmpty()) {
             return currentInput;
